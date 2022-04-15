6766ebf0274d9b52940f19840f572661a25f7c62
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-15 17:14:52 -0300

    feat(posts): add to posts main file new controller

--- src/controllers/posts/index.js
+++ src/controllers/posts/index.js
@@ -1,11 +1,13 @@
 const createPost = require('./createPost')
 const listPosts = require('./listPosts')
+const getPost = require('./getPost')
 const updatePost = require('./updatePost')
 const deletePost = require('./deletePost')
 
 module.exports = {
   createPost,
   listPosts,
+  getPost,
   updatePost,
   deletePost
 }
