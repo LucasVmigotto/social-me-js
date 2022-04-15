49e215a2a050e5b4877e92c22ebe6a2342ebb0d7
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-15 14:52:46 -0300

    feat(comments): add deleteComment method

---
+++ src/controllers/comments/deleteComment.js
@@ -0,0 +1,55 @@
+const { decamelizeKeys, camelizeKeys } = require('humps')
+const { makeResponse } = require('../../utils')
+
+module.exports = async ({ params, knex, logger, user }, res) => {
+  try {
+
+    const { commentId } = params
+
+    const [commentCreator] = await knex('social_me.comments')
+      .select('user_id')
+      .where('id', commentId)
+
+    const [postOwner] = await knex('social_me.posts')
+      .select('posts.user_id')
+      .leftJoin('social_me.comments', 'post_id', 'posts.id')
+      .where('comments.id', commentId)
+
+    const deletedBy = user.id === camelizeKeys(commentCreator).userId
+      ? 'COMMENT_CREATOR'
+      : user.id === camelizeKeys(postOwner).userId
+        ? 'POST_OWNER'
+        : null
+
+    if (!deletedBy) {
+
+      return makeResponse(
+        res,
+        409,
+        { message: 'Only comment creators or related post owners can remove comments' }
+      )
+
+    }
+
+    const [commentDeleted] = await knex('social_me.comments')
+      .update(decamelizeKeys({ deletedBy }))
+      .where('id', commentId)
+      .returning('id')
+
+    return makeResponse(
+      res,
+      200,
+      { message: 'Comment successfully deleted' }
+    )
+
+  } catch (err) {
+
+    logger.error(`Exception in deleteComment: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { message: 'An internal error occurred' }
+    )
+
+  }
+}
