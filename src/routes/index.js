bf86cb025dd8275b122d231adc57b78d8e74b8d5
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-15 16:40:13 -0300

    feat(routes): add reactions routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { authMe } = require('../controllers/auth')
 
 const users = require('../controllers/users')
 const posts = require('../controllers/posts')
+const reactions = require('../controllers/reactions')
 const comments = require('../controllers/comments')
 
 module.exports = app => {
@@ -18,6 +19,11 @@ module.exports = app => {
   app.get('/posts', protectedByAuth, posts.listPosts)
   app.put('/posts/:postId', protectedByAuth, posts.updatePost)
   app.delete('/posts/:postId', protectedByAuth, posts.deletePost)
+
+  app.patch('/posts/:postId/like', protectedByAuth, reactions.addReaction('likes'))
+  app.patch('/posts/:postId/like/undo', protectedByAuth, reactions.removeReaction('likes'))
+  app.patch('/posts/:postId/dislike', protectedByAuth, reactions.addReaction('dislikes'))
+  app.patch('/posts/:postId/dislike/undo', protectedByAuth, reactions.removeReaction('dislikes'))
 
   app.post('/comments', protectedByAuth, comments.createComment)
   app.get('/comments/:postId', protectedByAuth, comments.listComments)
