66821e31d6b140453eadc2796651135790d0b77f
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-15 14:52:36 -0300

    feat(comments): add updateComment method
    Minor fix in updatePost method

--- src/controllers/posts/updatePost.js
+++ src/controllers/posts/updatePost.js
@@ -7,6 +7,18 @@ module.exports = async ({ params, body, knex, logger, user }, res) => {
     const { postId } = params
     const { title, description } = body
 
+    if (!title || !description) {
+
+      return makeResponse(
+        res,
+        500,
+        !title
+          ? { message: 'Field \'title\' is missing from payload' }
+          : { message: 'Field \'description\' is missing from payload' }
+      )
+
+    }
+
     const [creator] = await knex('social_me.posts')
       .select('user_id')
       .where('id', postId)
@@ -17,18 +29,6 @@ module.exports = async ({ params, body, knex, logger, user }, res) => {
         res,
         409,
         { message: 'You can only edit post that you have created' }
-      )
-
-    }
-
-    if (!title || !description) {
-
-      return makeResponse(
-        res,
-        500,
-        !title
-          ? { message: 'Field \'title\' is missing from payload' }
-          : { message: 'Field \'description\' is missing from payload' }
       )
 
     }
