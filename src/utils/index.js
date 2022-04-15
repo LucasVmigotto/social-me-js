6bd8b0f93160619c3850e8453cf9e0967e8f361d
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-15 08:31:26 -0300

    fix(utils): add doc string to utils function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,15 @@
+/**
+ *
+ * Function that mount and return
+ * a response to the request where
+ * was used
+ *
+ * @param {object} response
+ * @param {number} code
+ * @param {object} payload
+ * @returns The express response to the related
+ * request
+ */
 const makeResponse = (response, code = 201, payload = {}) => response
   .status(code)
   .send({ ...payload})
